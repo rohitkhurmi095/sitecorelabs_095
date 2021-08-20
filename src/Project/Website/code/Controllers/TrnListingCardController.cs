b7f6b8053c21fefa980c27d17e1762a481e9312f
Gopi S V <gopisivanappan123@outlook.com>
2021-08-20 09:20:39 -0700

    EntityListingCard(Inheritance Concept) - Staffs/Students/Subjects{Name(url),Brief}

---
+++ src/Project/Website/code/Controllers/TrnListingCardController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sitecore;
+using Sitecore.Project.Website.Models;
+using Sitecore.Links;
+
+namespace Sitecore.Project.Website.Controllers
+{
+    /* Listing Card Component */
+    //-------------------------
+    //for Staffs/Students/Subjects ListingPage
+    /*Eg: Staffs(Listing)
+          - Staff1
+          - Staff2
+        - get Children under Staffs
+        - Sitecore.Context.Item.GetChildren()
+        - returns childList[](collection) 
+        - convert this childList[] -> List using .ToList()
+    */
+
+    public class TrnListingCardController : Controller
+    {
+        // GET: TrnListingCard
+        public ActionResult Index()
+        {
+            //contextItem - currentContext
+            var contextItem = Sitecore.Context.Item;
+
+            //singleListing card 
+            var listingCard = Sitecore.Context.Item.GetChildren()
+                              .Select(x => new DeptListingCard{
+                                  EntityName = x.Fields["EntityName"].Value,
+                                  EntityBrief = new HtmlString(x.Fields["EntityBrief"].Value),
+                                  EntityUrl = LinkManager.GetItemUrl(x)
+                              }).ToList();
+
+            //return listingCard to View
+            return View(listingCard);
+        }
+    }
+}
---
+++ src/Project/Website/code/Models/DeptListingCard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Project.Website.Models
+{
+    //Dept(single Staff/Student/Subject) - card
+    //Get Entity Name/Breif - BaseInfoTemplate + Inherit in Staff/Department/Subject Templates
+    //Displayed on Staff/Student/Subject ListingPage
+    //-----------------------------------------------
+    public class DeptListingCard
+    {
+        public string EntityName { get; set; }
+        public HtmlString EntityBrief { get; set; }
+        public string EntityUrl { get; set; }
+    }
+}
