81657fdac3e7d8b8b3e10ac506b2f96aa86897fd
Gopi S V <gopisivanappan123@outlook.com>
2021-08-20 04:07:22 -0700

    Created MVC for - Breadcrumb component

---
+++ src/Project/Website/code/Controllers/TrnBreadcrumbController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sitecore;
+using Sitecore.Project.Website.Models;
+using Sitecore.Links;
+using Sitecore.Data.Items;
+
+namespace Sitecore.Project.Website.Controllers
+{
+    public class TrnBreadcrumbController : Controller
+    {  
+
+        //LINQ Statement (Language Integrated Query)
+        //LINQ - Reduces the line of code in C#
+        //Where - Filtering Operation
+        //Select - Projection Operation
+        //----------------------------------------
+        //Breadcrumb => ItemName/ItemUrl - upto root
+        //Eg: Home/Articles/Article1 - list of ItemName/ItemUrl - List<BreadcrumbItem> 
+        /*context -> sitecore-demo.local.com -> Home -> Articles -> Article1 
+          contextItem = currentItem (Article1)
+           - get Ancestors of currentItem - Sitecore.Context.Item.Axes.GetAncestors() - returns array[ancestors]
+               - context -> sitecore-demo.local.com -> Home -> Articles
+           - But we want Items below Home only
+             - find StartItem(Home in siteDefination file) - Sitecore.Context.Database.GetItem(startPath)
+               startPath = Sitecore.Context.Site.StartPath 
+             - get the decendant of Home - Where(x => x.Axes.isDescendentOf(startItem))
+          - But this will not include current contextItem
+             - add current contextItem to this List[] - concat(new Item[]{Sitecore.Context.Item})
+          - Display result (Project) as per Model 
+            itrerate through a lis of ancestors & create a new object
+            Select(x => new BreadcrumbItem{
+                 ItemName = x.Name,
+                 ItemUrl = LinkManager.GetDynamicUrl(x)
+             })
+        - convert to List[] => ToList()*/
+
+
+        // GET: TrnBreadcrumb
+        public ActionResult Index() 
+        {
+            //current context
+            var contextItem = Sitecore.Context.Item;
+
+            //Home - startItem
+            var startItem = Sitecore.Context.Database.GetItem(Sitecore.Context.Site.StartPath);
+
+            //create model instance + set values 
+            //List of BreadcrumbItems(NavigationItems)
+            List<BreadcrumbItem> ancestors = Sitecore.Context.Item.Axes.GetAncestors()
+                                             .Where(x=> x.Axes.IsDescendantOf(startItem))
+                                             .Concat(new Item[] {contextItem})
+                                             .Select(x => new BreadcrumbItem {
+                                                 ItemName = x.Name,
+                                                 ItemUrl = LinkManager.GetDynamicUrl(x)
+                                             }).ToList();
+            
+            
+            //return model instance to view
+            return View(ancestors);
+        }
+    }
+}
---
+++ src/Project/Website/code/Models/BreadcrumbItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Project.Website.Models
+{
+    //Navigation Item Model
+    //-----------------------
+    public class BreadcrumbItem
+    {
+        public string ItemName { get; set; }
+        public string ItemUrl { get; set; }
+    }
+}
